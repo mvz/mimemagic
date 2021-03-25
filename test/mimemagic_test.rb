947e1f5ddbc9ea3a3e6a90c3eaa3c32c7ff91b0a
Matijs van Zuijlen <matijs@matijs.net>
2021-03-25 14:37:09 +0100

    Restore mimemagic/overlay.rb
    This overlay is needed to correctly detect Microsoft Office 2007+
    formats.

--- test/mimemagic_test.rb
+++ test/mimemagic_test.rb
@@ -78,6 +78,7 @@ class TestMimeMagic < Minitest::Test
   end
 
   def test_recognize_by_magic
+    load "mimemagic/overlay.rb"
     Dir['test/files/*'].each do |file|
       mime = file[11..-1].sub('.', '/').sub(/\{\w+\}/, '')
       assert_equal mime, MimeMagic.by_magic(File.read(file)).to_s
@@ -86,6 +87,7 @@ class TestMimeMagic < Minitest::Test
   end
 
   def test_recognize_all_by_magic
+    load 'mimemagic/overlay.rb'
     %w(msoffice rubyxl gdocs).each do |variant|
       file = "test/files/application.vnd.openxmlformats-officedocument.spreadsheetml{#{variant}}.sheet"
       mimes = %w[application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/zip]
